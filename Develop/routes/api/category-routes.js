34c060dc5e3fb14c81d984a0a9478eb29754dadb
Markki Meyer <markkimeyer@gmail.com>
2021-03-14 17:38:49 -0500

    updated cat-routes and model index.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,19 +3,50 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+
+
+
+router.get('/', async(req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: product }],
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'Sorry, no category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(CategorydData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+router.post('/', async(req, res) => {
+    try {
+      const CategoryData = await Category.create(req.body);
+      res.status(200).json(CategoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
