53d7a692810d33415a9f7ee81fe54ec79639f5ce
Markki Meyer <markkimeyer@gmail.com>
2021-03-15 20:00:38 -0500

    port listening

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -20,7 +20,7 @@ router.get('/', async(req, res) => {
 });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
@@ -39,7 +39,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     try {
       const CategoryData = await Category.create(req.body);
       res.status(200).json(CategoryData);
