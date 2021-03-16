ae235645cc4c7f76269d1bcadd44b56f8f0c35b5
Markki Meyer <markkimeyer@gmail.com>
2021-03-16 18:00:03 -0500

    tag routes updated

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -24,11 +24,11 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const CategoryData = await Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: product }],
     });
 
-    if (!CategoryData) {
+    if (!categoryData) {
       res.status(404).json({ message: 'Sorry, no category found with that id!' });
       return;
     }
@@ -41,8 +41,8 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const CategoryData = await Category.create(req.body);
-    res.status(200).json(CategoryData);
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
